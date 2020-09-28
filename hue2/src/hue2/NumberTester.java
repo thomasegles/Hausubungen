9e5c011a2998345dc2700727e256242d86774c28
thomasegles <71641615+thomasegles@users.noreply.github.com>
2020-09-28 14:51:43 +0200

    Setter functionality

--- hue2/src/hue2/NumberTester.java
+++ hue2/src/hue2/NumberTester.java
@@ -5,31 +5,42 @@
  */
 package hue2;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Thomas
  */
 public class NumberTester {
 
-    public NumberTester(String fileName) {
-    }
+    String fileName;
 
-    ;
+    NumberTest oddTester;
+    NumberTest primeTester;
+    NumberTest palindromeTester;
+
+    public NumberTester(String fileName) {
+        this.fileName = fileName;
+    }
 
     public void setOddEvenTester(NumberTest oddTester) {
+        this.oddTester = oddTester;
     }
 
-    ;
     public void setPrimeTester(NumberTest primeTester) {
+        this.primeTester = primeTester;
     }
 
-    ;
     public void setPalindromeTester(NumberTest palindromeTester) {
+        this.palindromeTester = palindromeTester;
     }
-
-    ;
 
     public void testFile() {
     }
-;
+
 }
