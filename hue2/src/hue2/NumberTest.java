b542af9c82abaa035507a25dd010e85e1eddaf7f
thomasegles <71641615+thomasegles@users.noreply.github.com>
2020-09-28 14:05:30 +0200

    NumberTest classes and methods

---
+++ hue2/src/hue2/NumberTest.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hue2;
+
+/**
+ *
+ * @author Thomas
+ */
+public interface NumberTest {
+
+    boolean testNumber(int number);
+}
